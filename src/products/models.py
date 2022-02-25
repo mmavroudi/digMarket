b368a4d83781271c150a64047128f39d654b5683
mmavroudi <mmavroudi@gmail.com>
2022-02-25 22:31:11 +0200

    Create Products model #3

--- src/products/models.py
+++ src/products/models.py
@@ -5,10 +5,10 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField()
-    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99) #100.00
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99, null=True) #100.00
     sale_price = models.DecimalField(max_digits=100,
                                      decimal_places=2, default=6.99, null=True, blank=True)  # 100.00
 
-    def _str_(self): #def_unicode_(self) in Python 2
+    def __str__(self): #def_unicode_(self) in Python 2
         return self.title
 
