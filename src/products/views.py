82992f5b530a9a46353ce22517e496d780303174
katerina <kkampardi@gmail.com>
2022-03-09 19:32:10 +0200

    22 Add slug fields to product detail url #6

--- src/products/views.py
+++ src/products/views.py
@@ -6,6 +6,21 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
+def detail_slug_view(request, slug=None):
+    product = Product.objects.get(slug=slug)
+    try:
+        product = get_object_or_404(Product, slug=slug)
+    except Product.MultipleObjectsReturned:
+        product = Product.objects.filter(slug=slug).order_by("-title").first()
+    # print slug
+    # product = 1
+    template = "detail_view.html"
+    context = {
+        "object": product
+        }
+    return render(request, template, context)
+
+
 def detail_view(request, object_id=None):
     product = get_object_or_404(Product, id=object_id)
     template = "detail_view.html"
@@ -13,15 +28,6 @@ def detail_view(request, object_id=None):
         "object": product
     }
     return render(request, template, context)
-    # if object_id is not None:
-    # 	product = get_object_or_404(Product, id=object_id)
-    #     product = Product.objects.get(id=object_id)
-    #     try:
-    #         product = Product.objects.get(id=object_id)
-    #     except Product.DoesNotExist:
-    #         product = None
-    # else:
-    #     raise Http404
 
 
 def list_view(request):
