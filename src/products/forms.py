7c0650b041d7f63cc46c937330cd9f4a51b24985
mmavroudi <mmavroudi@gmail.com>
2022-03-15 17:46:37 +0200

    Forms, Validation for those forms & Widgets #13

---
+++ src/products/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+PUBLISH_CHOICES = (
+    #('', ""),
+    ('publish', "Publish"),
+    ('draft', "Draft")
+)
+class ProductAddForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(widget=forms.Textarea)
+    price = forms.DecimalField()
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00:
+            raise forms.ValidationError("Price must be greater than $1.00")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be less than $100")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long.")
