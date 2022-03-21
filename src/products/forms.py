9af2a489987d21e82cb9bc61806884ab45468098
mmavroudi <mmavroudi@gmail.com>
2022-03-21 13:12:47 +0200

    Model forms #13

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Product
+
 PUBLISH_CHOICES = (
     #('', ""),
     ('publish', "Publish"),
@@ -36,3 +38,30 @@ class ProductAddForm(forms.Form):
         else:
             raise forms.ValidationError("Title must be greater than 3 characters long.")
 
+
+
+class ProductModelForm(forms.ModelForm):
+    publish = forms.ChoiceField(widget=forms.RadioSelect, choices=PUBLISH_CHOICES, required=False)
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00:
+            raise forms.ValidationError("Price must be greater than $1.00")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be less than $100")
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long.")
