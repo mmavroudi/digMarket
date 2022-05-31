2cc52e53f714732d6bcffd353e8bd541a6c54725
mmavroudi <mmavroudi@gmail.com>
2022-05-31 16:51:18 +0300

    New changes that needed to be done #23

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^products/', include("products.urls", namespace='products')),
