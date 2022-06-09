7965a569ce32063fc3f5b8a71f2eaf2925d5b497
mmavroudi <mmavroudi@gmail.com>
2022-06-09 10:43:08 +0300

    Setup staticfiles, locally serving static and media files #26

--- src/digitalmarket/settings.py
+++ src/digitalmarket/settings.py
@@ -101,3 +101,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+#TEAM MANAGES
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+
+
+
+)
+
+#TEAM DOESN'T
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "staticfiles")  #'collect files here'  #CDN /AWS S3 BUCKET
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "mediafiles")
+
+
+
+
+
+
+
+
+
+
+
--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -1,9 +1,20 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from .views import home_view as home_view
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^$', home_view, name='home_view'),
 ]
+
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
